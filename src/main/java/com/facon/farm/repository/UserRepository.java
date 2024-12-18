4b7012c60e480ca2c6e963dee21cafdb41c0abb8
keep-moving <125468239+Dream-drive@users.noreply.github.com>
2024-12-18 23:54:16 +0000

    Delete src/main/java/com/facon/farm directory

--- src/main/java/com/facon/farm/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.facon.farm.repository;
-
-import com.facon.farm.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    // Custom method to find a user by email
-    User findByEmail(String email);
-}
